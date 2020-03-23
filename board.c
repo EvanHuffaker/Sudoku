0c0d35f05260430e544133e72811fcfa833b15fd
EvanHuffaker <58517381+EvanHuffaker@users.noreply.github.com>
2020-03-23 15:13:43 -0400

    Add files via upload

--- board.c
+++ board.c
@@ -2,11 +2,45 @@
 
 
 void _create_cells_(unsigned short bd[DIM][DIM], cell cells[DIM][DIM]){
-	int i,j;
+	unsigned short i,j;
 	for(i = 0; i < DIM; i++){
 		for(j = 0; j < DIM; j++){
 			cells[i][j].value = bd[i][j];
 			cells[i][j].candidates = 0;
+		}
+	}
+}
+void create_board(unsigned short cells[DIM][DIM], board *bd){
+	_create_cells(cells,bd->cells);
+	int i;
+	for(i = 0; i < 9; i++){
+		_create_rows_(bd,i);
+		_create_cols_(bd,i);
+		_create_block_(bd,i);
+	}
+}
+
+void _create_rows_(board* bd, unsigned short row){
+	unsigned short i;
+	for(i = 0; i < DIM; i++){
+		bd->rows[row]->items[i] = bd->cells[row][i];
+	}
+}
+
+void _create_cols_(board* bd, unsigned short col){
+	unsigned short i;
+	for(i = 0; i < DIM; i++){
+		bd->cols[col]->items[i] = bd->cells[i][col];
+	}
+}
+
+void _create_blocks_(board* bd, unsigned short blk){
+	unsigned short i,j, istart, jstart;
+	istart = (blk/3)*3;
+	jstart = (blk%3)*3;
+	for(i = istart; i < istart+3; i++){
+		for(j = jstart; j < jstart+3; j++){
+			bd->blocks[blk]->items[(i-istart)*3 + j-jstart] = bd->cells[i][j];
 		}
 	}
 }
--- board.h
+++ board.h
@@ -11,10 +11,23 @@ typedef struct _cell_{
 	unsigned short candidates;			//stored in each bit
 }cell;
 
+typedef struct _group_{
+	cell* items[DIM];
+}group;
+
+typedef struct _board_{
+	group rows[DIM];
+	group cols[DIM];
+	group blocks[DIM];
+	cell cells[DIM][DIM];
+}board;
 
 
 
 /* Allocation functions */
-void create_board(unsigned short cells[DIM][DIM]);
+void create_board(unsigned short cells[DIM][DIM], board *bd);
 void _create_cells_(unsigned short bd[DIM][DIM], cell cells[DIM][DIM]);
+void _create_rows_(board* bd, unsigned short row);								//Assigns one value of rows in the board structure.  (9 total cells)
+void _create_cols_(board* bd, unsigned short col);
+void _create_blocks_(board* bd, unsigned short blk);
 #endif /* BOARD_H_ */
